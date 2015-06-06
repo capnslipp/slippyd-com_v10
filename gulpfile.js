f0675df5723cb5138f0592ad16a4489bc27b9005
Slipp Douglas Thompson <git@slippyd.com>
2015-06-06 19:39:09 -0400

    Modified SASS source/dest paths
    * Added new `source/` dir for all source-code files, and `compiled/` dir for outputted, browser-read files.  
    	» I’m not a big fan of separating human-created files by type— that’s kind of a problem well-solved by the “Kind” sortable column heading in OS X’s Finder (or equiv).
    	* Added `compiled/css/` sub-dir for CSS output files.
    * Added a `const PATHS` const-var to the gulpfile to hold all paths to different files in the project.  
    	» Creates the object via a `createConstPATHS()` function in order to be able to keep the dir & file glob combinations a bit DRYer.
    * Modified the `gulp.src(…`, `gulp.dest(…`, and `gulp.watch(…` args to the paths for the source (`PATHS.dev.sass.glob`) and dest (`PATHS.web.css.dir`).
    * New `source/main.scss` test file to make sure the Gulpfile is all working, and `compiled/css/main.css` output file.

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,46 @@ var sass = require('gulp-sass');
 
 
 
+// Constants
+
+const PATHS = (function createConstPATHS()
+{
+	var sass_dir = './source/**/';
+	var sass_globs = [
+		sass_dir+'*.sass',
+		sass_dir+'*.scss'
+	];
+	
+	var css_dir = './compiled/css/';
+	var css_glob = css_dir+'*';
+	
+	function Path(dir_or_dirs, glob_or_globs) {
+		this.dir = dir_or_dirs;
+		this.glob = glob_or_globs;
+	}
+	
+	return {
+		dev: {
+			sass: new Path(sass_dir, sass_globs),
+		},
+		web: {
+			css: new Path(css_dir, css_glob),
+		},
+	};
+})();
+
+
+
+// Tasks
+
 gulp.task('sass', function () {
-	gulp.src('./sass/**/*.scss').pipe(
+	gulp.src(PATHS.dev.sass.glob).pipe(
 		sass().on('error', sass.logError)
 	).pipe(
-		gulp.dest('./css')
+		gulp.dest(PATHS.web.css.dir)
 	);
 });
 
 gulp.task('sass:watch', function () {
-	gulp.watch('./sass/**/*.scss', ['sass']);
+	gulp.watch(PATHS.dev.sass.glob, ['sass']);
 });
